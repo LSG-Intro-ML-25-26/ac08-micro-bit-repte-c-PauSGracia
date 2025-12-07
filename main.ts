d47f812149df208480e73bd57e26f73b37789d5e
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-07 15:18:58 +0100

    add send message

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hola")
+    basic.showString("SENT")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
